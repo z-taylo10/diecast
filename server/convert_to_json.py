f842802c949e464eb96048276da18872072ebfc0
Zane <ztbugatti01@hotmail.com>
2024-07-19 19:01:04 -0700

    excel to json bugfixxing 2

--- server/convert_to_json.py
+++ server/convert_to_json.py
@@ -4,9 +4,10 @@ import json
 
 def convert_excel_to_json(excel_file, json_file):
     df = pd.read_excel(excel_file)
-    data = df.to_dict(orient='records')
+    # Convert DataFrame to JSON with NaN as null
+    data = df.to_json(orient='records', date_format='iso', default_handler=str)
     with open(json_file, 'w') as f:
-        json.dump(data, f, indent=2)
+        f.write(data)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
